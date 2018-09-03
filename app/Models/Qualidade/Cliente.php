6d7205fe83e87933e022fe2468bc7db237efe594
Joeliton Vieira <joelitonmatos@gmail.com>
2018-09-03 20:35:34 -0300

    Criando Models de Produto e Cliente para Estoque

---
+++ app/Models/Qualidade/Cliente.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\\Qualidade;
+
+use Illuminate\Database\Eloquent\Model;
+
+class \Cliente extends Model
+{
+    //
+}
---
+++ app/Models/Qualidade/Produto.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\\Qualidade;
+
+use Illuminate\Database\Eloquent\Model;
+
+class \Produto extends Model
+{
+    //
+}
