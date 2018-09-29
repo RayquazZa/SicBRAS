d747e6b97a676376cd35860401af7dd97388adfa
Matheus Trindade <trindadematheus27@gmail.com>
2018-09-29 01:22:39 -0300

    Adicionando mascara que remove oque nao for letras

--- resources/views/vendor/adminlte/register.blade.php
+++ resources/views/vendor/adminlte/register.blade.php
@@ -19,7 +19,7 @@
                 {!! csrf_field() !!}
 
                 <div class="form-group has-feedback {{ $errors->has('name') ? 'has-error' : '' }}">
-                    <input type="text" name="name" class="form-control" value="{{ old('name') }}"
+                    <input type="text" name="name" id="campoNome" class="form-control" value="{{ old('name') }}"
                            placeholder="{{ trans('adminlte::adminlte.full_name') }}">
                     <span class="glyphicon glyphicon-user form-control-feedback"></span>
                     @if ($errors->has('name'))
@@ -69,8 +69,16 @@
         </div>
         <!-- /.form-box -->
     </div><!-- /.register-box -->
+    
 @stop
 
 @section('adminlte_js')
     @yield('js')
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('#campoNome').keyup(function() {
+                $(this).val(this.value.replace(/\D/g, ''));
+            });
+        }); 
+    </script>
 @stop
